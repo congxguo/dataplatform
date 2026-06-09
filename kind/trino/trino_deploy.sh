1a21619de3a0f166cd71da1ef2676e38d9773695
Cong Guo <cong.guo@smartnews.com>
2026-06-09 11:30:47 +0800

    feat: for trino deployment

---
+++ kind/trino/trino_deploy.sh
@@ -0,0 +1,12 @@
+helm repo add trino https://trinodb.github.io/charts
+
+helm repo update
+
+kubectl create namespace trino
+
+helm install trino trino/trino \
+  -n trino \
+  --create-namespace \
+  -f values.yaml
+
+kubectl port-forward svc/trino 8080:8080
